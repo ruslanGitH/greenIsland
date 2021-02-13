8d3b728aba07f6cc4f46800ad8937f51e1d5cd31
Ruslan <rus102kr@yandex.ru>
2021-02-13 15:31:20 +0300

    update login1

--- src/main/java/com/shop/config/SimpleCorsFilter.java
+++ src/main/java/com/shop/config/SimpleCorsFilter.java
@@ -11,7 +11,7 @@ public class SimpleCorsFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "https://garden-green-a48b8.web.app/*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
