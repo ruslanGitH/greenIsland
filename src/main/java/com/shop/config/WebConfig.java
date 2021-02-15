e69cdf0569d3aff1e1180dc9cfd591272feebc7d
Ruslan <rus102kr@yandex.ru>
2021-02-15 22:46:18 +0300

    allowed methods delete

--- src/main/java/com/shop/config/WebConfig.java
+++ src/main/java/com/shop/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 }
